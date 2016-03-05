3f7853631b68c7df5f5439d35ff418868dca11aa
richard <reigun11@gmail.com>
2016-03-05 15:31:39 -0300

    Colocando SQL Lite Banco de dados

---
+++ app/src/main/java/br/com/trainning/pdv/PdvApplication.java
@@ -0,0 +1,44 @@
+package br.com.trainning.pdv;
+
+import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import se.emilsjolander.sprinkles.Migration;
+import se.emilsjolander.sprinkles.Sprinkles;
+
+/**
+ * Created by android on 05/03/2016.
+ */
+public class PdvApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Sprinkles sprinkles = Sprinkles.init(getApplicationContext());
+
+        sprinkles.addMigration(new Migration() {
+            @Override
+            protected void onPreMigrate() {
+                // do nothing
+            }
+
+            @Override
+            protected void doMigration(SQLiteDatabase db) {
+                db.execSQL(
+                        "CREATE TABLE Notes (" +
+                                "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                                "title TEXT,"+
+                                "body TEXT"+
+                                ")"
+                );
+            }
+
+            @Override
+            protected void onPostMigrate() {
+                // do nothing
+            }
+        });
+    }
+
+}

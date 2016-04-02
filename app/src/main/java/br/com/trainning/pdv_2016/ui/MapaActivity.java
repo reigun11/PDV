d22d84cf25d82bb23e6cbd2b02a4f7a0dec68df5
reigun11 <reigun11@gmail.com>
2016-04-02 10:04:26 -0300

    Incluindo layout do MAP BOX junto com o token que foi gerado no site https://www.mapbox.com/studio/account/apps

---
+++ app/src/main/java/br/com/trainning/pdv_2016/ui/MapaActivity.java
@@ -0,0 +1,18 @@
+package br.com.trainning.pdv_2016.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.trainning.pdv_2016.R;
+
+public class MapaActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+    }
+}
+
+
+
